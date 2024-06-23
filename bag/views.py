788e063c625f9e9945f40b6ff985079ba97be512
pbrdys <142018441+pbrdys@users.noreply.github.com>
2024-06-23 10:07:04 +0200

    Adding Products Part 2

--- bag/views.py
+++ bag/views.py
@@ -12,11 +12,13 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    # retrieve the current shopping bag from the user's session. 
+    # If the bag does not exist in the session, it initializes bag as an empty dictionary {}.
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
-    else:
+    else: # If it is not, it adds item_id to the bag dictionary with the value quantity.
         bag[item_id] = quantity
 
     request.session['bag'] = bag
