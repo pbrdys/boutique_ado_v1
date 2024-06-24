ef2fae5a922ef1426b5c5f01702452f6414d61c8
pbrdys <142018441+pbrdys@users.noreply.github.com>
2024-06-24 16:26:09 +0200

    Adding Products Part 4

--- bag/contexts.py
+++ bag/contexts.py
@@ -12,15 +12,30 @@ def bag_contents(request):
     
     # iterating through key value pair (i can name them however i want)
     # for theKey, theValue in bag.items():
-    for item_id, quantity in bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    for item_id, item_data in bag.items():
+        # if item_data is an int, we know it is just the quantity
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            # otherwise it is the dictionary and we have to work differently
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                    'size': size,
+                })
+            
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
--- bag/views.py
+++ bag/views.py
@@ -12,14 +12,33 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']   
     # retrieve the current shopping bag from the user's session. 
     # If the bag does not exist in the session, it initializes bag as an empty dictionary {}.
     bag = request.session.get('bag', {})
-
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
-    else: # If it is not, it adds item_id to the bag dictionary with the value quantity.
-        bag[item_id] = quantity
+    
+    # if product has_sizes
+    if size:
+        # if item with the id is already in the bag, then ...
+        if item_id in list(bag.keys()):
+            # check if the existing item exists with the same size or not
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            # if item not existing, add it as dictionary to bag[item_id]
+            # because we can have items with the same id but different sizes in our bag
+            bag[item_id] = {'items_by_size': {size: quantity}}
+    else:
+        # else if product has no sizes
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else: # If it is not, it adds item_id to the bag dictionary with the value quantity.
+            bag[item_id] = quantity   
 
     request.session['bag'] = bag
     return redirect(redirect_url)
